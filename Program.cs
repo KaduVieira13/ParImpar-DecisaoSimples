21868faa77dae8ea435f5d8bb48a193d73e4e430
KaduVieira13 <carloseduardo_alencar@outlook.com>
2021-04-19 10:34:49 -0300

    Programa finalizado

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ParImpar_DecisaoSimples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- Par ou Impar ---\n");
+
+            int numero;
+            bool numeroDigitado;
+
+            Console.Write("Digite um numero: ");
+            numeroDigitado = Int32.TryParse((Console.ReadLine()), out numero);
+
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine("O numero digitado é par.");
+            }
+            else
+            {
+                Console.WriteLine("O numero digitado é ímpar.");
+            }
+
+
+        }
+    }
+}
